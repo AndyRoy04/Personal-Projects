d2558626fb618957211a9b9f1b4370364f8b37ec
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-13 15:12:31 +0100

    Added all necessary functions

--- Calculator/calcu.js
+++ Calculator/calcu.js
@@ -32,98 +32,80 @@ function modulo(numb1, numb2) {
     return answer;
 }
 
-const result = document.getElementsByClassName('result');
+document.addEventListener('DOMContentLoaded', function () {
+
+    let number1 = '';
+    let numeber2 = '';
+    let operand = '';
+    let resultDisplay = false;
+
+    function Display(value) {
+        $(".b1 h1").text(value);
+    }
+
+    function append(characters) {
+        number1 += characters;
+        Display(number1);
+    }
+
+    function clear() {
+        let number1 = '';
+        let number2 = '';
+        let operand = '';
+        let resultDisplay = false;
+        Display('');
+    }
+
+    function calculate() {
+        let result;
+        if (operand !== '' && number1 !== '') {
+            if (operand === 'x²') {
+                result = xSquared(Number(number1));
+            } else if (operand === '✓x') {
+                result = squareRoot(Number(number1));
+            } else if (operand === '1/x') {
+                result = oneX(Number(number1));
+            }
+        } else if (operand !== '' && number2 !== '') {
+            if (operand === '+') {
+                result = addition(Number(number1), Number(number2));
+            } else if (operand === '-') {
+                result = subtraction(Number(number1), Number(number2));
+            } else if (operand === '/') {
+                result = division(Number(number1), Number(number2));
+            } else if (operand === '*') {
+                result = multiplication(Number(number1), Number(number2));
+            } else if (operand === '%') {
+                result = modulo(Number(number1), Number(number2));
+            }
+        }
+    }
+
+
+
+
+    $(".number").click(function () {
+        var buttonClicked = $(this).text();
+        number1 += buttonClicked;
+        $("h1").text(parseFloat(number1));
+
+
+
+    });
+
+
+    $('.result').click(function () {
+        var result = $(this).text();
+        $("h1").text(result);
+    })
+
+    $('.operator').click(function () {
+        var operand = $(this).text();
+        $("h1").text(operand);
+        console.log($('operator'))
+    })
 
-setTimeout(function() {
-    document.getElementsByClassName('slash').innerHTML = '|';
-  }, 1000);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-const operators = ["1/x", "x²", "✓x", "/", "*", "-", "+", "±", ".", "%", "AC", "="];
-var Clicked = [];
-$("button").click(function () {
-    // var buttonClicked = $(this).text();
     // Clicked.push(buttonClicked);
 
     // const listString = Clicked.join('');
@@ -146,46 +128,48 @@ $("button").click(function () {
     //     }
     // }
 
-});
-
-function operation(value, numb1) {
-    switch (value) {
-        case "1/x":
-            $("h1").text(oneX(numb1));
-            break;
-        case "x²":
-            $("h1").text(xSquared(numb1));
-            break;
-        case "✓x":
-            $("h1").text(squareRoot(numb1));
-            break;
-        case "/":
-            $("h1").text(division(numb1));
-            break;
-        case "*":
-            $("h1").text(multiplication(numb1));
-            break;
-        case "-":
-            $("h1").text(subtraction(numb1));
-            break;
-        case "+":
-            $("h1").text(addition(numb1));
-            break;
-        case "%":
-            $("h1").text(modulo(numb1));
-            break;
-        case "±":
-            // $("h1").text((numb1));
-            break;
-        case "AC":
-            // $("h1").text(oneX(numb1));
-            break;
-        case "=":
-            // $("h1").text(xSquared(numb1));
-            break;
 
 
-        default:
-            break;
-    }
-}
+    // function operation(value, numb1) {
+    //     switch (value) {
+    //         case "1/x":
+    //             $("h1").text(oneX(numb1));
+    //             break;
+    //         case "x²":
+    //             $("h1").text(xSquared(numb1));
+    //             break;
+    //         case "✓x":
+    //             $("h1").text(squareRoot(numb1));
+    //             break;
+    //         case "/":
+    //             $("h1").text(division(numb1));
+    //             break;
+    //         case "*":
+    //             $("h1").text(multiplication(numb1));
+    //             break;
+    //         case "-":
+    //             $("h1").text(subtraction(numb1));
+    //             break;
+    //         case "+":
+    //             $("h1").text(addition(numb1));
+    //             break;
+    //         case "%":
+    //             $("h1").text(modulo(numb1));
+    //             break;
+    //         case "±":
+    //             // $("h1").text((numb1));
+    //             break;
+    //         case "AC":
+    //             // $("h1").text(oneX(numb1));
+    //             break;
+    //         case "=":
+    //             // $("h1").text(xSquared(numb1));
+    //             break;
+
+
+    //         default:
+    //             break;
+    //     }
+    // }
+
+})
