d0218b8d19abfe1825081a1c0a0fd59e3c036bdb
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-16 22:34:54 +0100

    Made clock run Infinitely

--- Time/script.js
+++ Time/script.js
@@ -1,7 +1,26 @@
 
-// const body = document.querySelector('body')
+// setInterval Permits the clock to run infinitely
+setInterval(() => {
+    let time = document.querySelector('h2')
+    let date = new Date();
+    let hour = date.getHours();
+    let minute = date.getMinutes();
+    let second = date.getSeconds();
 
+    if (hour < 10) {
+        hour = "0" + hour;
+    }
+    if (minute < 10) {
+        minute = "0" + minute;
+    }
+    if (second < 10) {
+        second = "0" + second;
+    }
+
+    time.innerHTML = hour + ":" + minute + ":" + second;
+    document.querySelector('strong').innerHTML = second;
+});
 
 // setTimeout(function(){
-//    body.classList.add('dark') 
+//    body.classList.add('dark')
 // }, 2000)
