f901175bb0fab7c35dcd99ab0e1402496dbbcd68
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-17 03:39:03 +0100

    Complete clock

--- Time/script.js
+++ Time/script.js
@@ -1,6 +1,8 @@
 const body = document.querySelector('body');
 const strong = document.querySelector('strong');
-const lastParagraph = document.querySelector('.second'); 
+const lastParagraph = document.querySelector('.second');
+const svg = document.querySelector('.first'); 
+const text = document.querySelector('h3');
 
 // setInterval Permits the clock to run infinitely
 setInterval(() => {
@@ -23,12 +25,26 @@ setInterval(() => {
     time.innerHTML = hour + ":" + minute + ":" + second;
     strong.innerHTML = hour;
 
+    if(hour >= 22 || hour < 6){
+        text.innerHTML = 'Get some rest 🛌🏿';
+    }else if(hour >= 6 && hour <10){
+        text.innerHTML = 'Time to be yourself 😁';
+    } else if(hour >= 10 && hour < 22){
+        text.innerHTML = "It's time to Work 💻"
+    }
+    else{
+        text.innerHTML = 'wcdedwec'
+    }
+
     if (hour >= 6 && hour < 18) {
-        body.classList.remove('dark')
-        lastParagraph.innerHTML = " Day Light"
+                body.classList.remove('dark')
+        lastParagraph.innerHTML = "Day Light"
+        svg.innerHTML = '<i class="bi bi-brightness-high-fill"></i>'
     } else {
         body.classList.add('dark')
-        lastParagraph.innerHTML = " Dark Time"
+        lastParagraph.innerHTML = "Dark theme"
+        svg.classList.add('grey')
+        svg.innerHTML = '<i class="bi bi-moon-stars-fill"></i>'
     }
 
     
