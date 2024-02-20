baedbc9815eec5930ae635c0843f1757ad51d474
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-20 17:03:17 +0100

    Collect date and time in Javascript

--- Alarm/script.js
+++ Alarm/script.js
@@ -1,14 +1,26 @@
 
-setInterval(() => {
-    let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
-    let second = date.getSeconds();
+// setInterval(() => {
+//     let date = new Date();
+//     let hour = date.getHours();
+//     let minute = date.getMinutes();
+//     let second = date.getSeconds();
 
-    if(second < 30) {
-        setTimeout(() => {
-            let audio = new Audio("./sounds/Jujutsu_Kaisen_AMV_《ANIMALS》(128k).mp3")
-            audio.play();
-        }, 5000);
-    }
-})
+//     if(second < 30) {
+//         setTimeout(() => {
+//             let audio = new Audio("./sounds/Jujutsu_Kaisen_AMV_《ANIMALS》(128k).mp3")
+//             audio.play();
+//         }, 5000);
+//     }
+// })
+
+const form = document.getElementById("myForm");
+
+form.addEventListener('submit', () => {
+
+
+    const dateInput = document.getElementById("dateInput").value;
+    const timeInput = document.getElementById("timeInput").value;
+    
+    const details = document.querySelector('h3')
+    details.innerHTML = `${dateInput} : ${timeInput}`;
+});
