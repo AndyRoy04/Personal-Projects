eaa812e24bfe16018da91f8fc0d102d92228d1de
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-22 20:13:38 +0100

    Taking Input from user

--- Alarm/script.js
+++ Alarm/script.js
@@ -1,28 +1,40 @@
 const form = document.getElementById("myForm");
-const dateInput = document.getElementById("dateInput").value;
+const setTime = document.getElementById('time');
+const setDate = document.getElementById('date');
 const timeInput = document.getElementById("timeInput").value;
 const details = document.querySelector('h3')
 
 setInterval(() => {
     let date = new Date();
-    let hour = date.getHours();
-    let minute = date.getMinutes();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
     let day = date.getDate();
-    let month = date.getMonth()+1;
+    let month = date.getMonth() + 1;
     let year = date.getFullYear();
+    let dayString = date.toDateString().slice(0, 3);
 
-    details.innerHTML = `${year}: ${month}`;
+    if (hours < 10) {
+        hours = '0' + hours;
+    }
+    if (minutes < 10) {
+        minutes = '0' + minutes;
+    }
+    if (seconds < 10) {
+        seconds = '0' + seconds;
+    }
+
+    setTime.innerHTML = `${hours} : ${minutes} : ${seconds}`;
+    setDate.innerHTML = `${date.toDateString()}`;
 
 })
-// document.getElementById('playsound').addEventListener('click', function(){
-//     playSound(10000);    
-// });
 
-// function playSound(time) {
-//     var audio = new Audio("./sounds/Karatenai(128k).mp3");
-//     audio.play();
-//     setTimeout(function() {
-//         audio.pause();
-//         audio.currentTime = 0;
-//     }, time);
-// }
+document.getElementById('playsound').addEventListener('click', function () {
+
+    var audio = new Audio("./sounds/Karatenai(128k).mp3");
+    audio.play();
+    setTimeout(function () {
+        audio.pause();
+        audio.currentTime = 0;
+    }, 10000);
+});
