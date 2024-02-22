8e94529a2a3bc3f0e2097f2ff869210736ae7f76
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-02-22 22:49:12 +0100

    To complete play sound

--- Alarm/script.js
+++ Alarm/script.js
@@ -1,18 +1,16 @@
+// Setting HTML input element
 const form = document.getElementById("myForm");
 const setTime = document.getElementById('time');
 const setDate = document.getElementById('date');
 const timeInput = document.getElementById("timeInput").value;
 const details = document.querySelector('h3')
 
+// Setting script variables to be used
 setInterval(() => {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
-    let day = date.getDate();
-    let month = date.getMonth() + 1;
-    let year = date.getFullYear();
-    let dayString = date.toDateString().slice(0, 3);
 
     if (hours < 10) {
         hours = '0' + hours;
@@ -27,14 +25,29 @@ setInterval(() => {
     setTime.innerHTML = `${hours} : ${minutes} : ${seconds}`;
     setDate.innerHTML = `${date.toDateString()}`;
 
+
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+
+    
+        const timeInput = document.getElementById("timeInput").value;
+        let alarmHour = timeInput.slice(0, 2);
+        let alarmMin = timeInput.slice(3, 5);
+        
+        details.innerHTML = `${alarmHour} and ${alarmMin}`;
+        if(alarmHour === hours && alarmMin === minutes){
+            playSound
+        }
+    })
 })
 
-document.getElementById('playsound').addEventListener('click', function () {
 
+// Play song function
+function playSound(){ 
     var audio = new Audio("./sounds/Karatenai(128k).mp3");
     audio.play();
     setTimeout(function () {
         audio.pause();
         audio.currentTime = 0;
     }, 10000);
-});
+}
