303fc3c261c3a348f589b1b1b51d6d61453aa074
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-03-02 23:45:10 +0100

    Passing HTML input values to a list

--- ToDo/script.js
+++ ToDo/script.js
@@ -2,9 +2,12 @@ const toDo = document.getElementById('toDo');
 const add = document.getElementById('add');
 const post = document.getElementById('post');
 
+const toDoList = [];
+
 add.addEventListener('click', function(){
     if(toDo.value !== ''){        
-        post.innerHTML = toDo.value;
+        toDoList.push(toDo.value);
         toDo.value = '';
     }
+    post.value = toDoList;
 })
