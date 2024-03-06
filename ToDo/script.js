19b1f472e10781283f67b873d15ca40b617225ec
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-03-06 21:29:27 +0100

    Added task completion icon

--- ToDo/script.js
+++ ToDo/script.js
@@ -1,5 +1,7 @@
 const toDo = document.getElementById('toDo');
+const toSearch = document.getElementById('toSearch');
 const add = document.getElementById('add');
+const search = document.getElementById('search');
 const post = document.getElementById('post');
 
 // const toDoList = [];
@@ -10,15 +12,33 @@ add.addEventListener('click', function(){
         var taskItem = document.createElement('li');
         var taskText = document.createTextNode(description);
         var trash = document.createElement('i');
+        var done = document.createElement('i');
+        done.classList.add('fas');
+        done.classList.add('fa-edit');
         trash.classList.add('fas');
         trash.classList.add('fa-trash-alt');
 
         taskItem.appendChild(taskText);
         taskItem.appendChild(trash);
+        taskItem.appendChild(done);
         post.appendChild(taskItem);
+
         toDo.value = '';
     }
 
+})
+
+search.addEventListener('click', function(e){
+    var searchDescription = toSearch.value.trim();
+
+    // To work on search property
+
+    // if(e.target.value == searchDescription){
+    //     var taskItem = e.target.parentNode;
+    //     post.value.animate({opacity: 1}, {duration: 3000});
+    // }
+
+    toSearch.value = '';
 })
 
 post.addEventListener('click', function(e){
