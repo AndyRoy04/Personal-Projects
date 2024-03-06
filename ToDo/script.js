104a22813f0e906637fb8169df6a17bd9dd1cf95
AndyRoy04 <118135786+AndyRoy04@users.noreply.github.com>
2024-03-06 20:37:41 +0100

    Made task view functional

--- ToDo/script.js
+++ ToDo/script.js
@@ -2,12 +2,24 @@ const toDo = document.getElementById('toDo');
 const add = document.getElementById('add');
 const post = document.getElementById('post');
 
-const toDoList = [];
+// const toDoList = [];
 
 add.addEventListener('click', function(){
-    if(toDo.value !== ''){        
-        toDoList.push(toDo.value);
+    var description = toDo.value.trim()
+    if(description !== ''){   
+        var taskItem = document.createElement('li');
+        var taskText = document.createTextNode(description);
+        var delButton = document.createElement('button');
+        var trash = document.createElement('i');
+        // trash.classList.add('fas');
+        // trash.classList.add('fa-trash-alt');
+        // delButton.appendChild(trash);
+        // delButton.classList.add('delete-button');
+
+        taskItem.appendChild(taskText);
+        taskItem.appendChild(trash);
+        post.appendChild(taskItem);
         toDo.value = '';
     }
-    post.value = toDoList;
+
 })
